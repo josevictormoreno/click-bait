9a36f77e34af7312f39f76a672ebb632006c96bf
josevictormoreno <josevictor.admoreno@gmail.com>
2023-06-11 02:26:13 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+import random 
+
+screen = tk.Tk()
+screen.title("ACEITA!")
+screen.geometry("600x600")
+screen.configure(background='#ffc8dd')
+
+def move_button(e):
+    if abs(e.x - button_1.winfo_x()) < 50 and abs(e.y - button_1.winfo_y()) < 40:
+        x = random.randint(0, screen.winfo_width() - button_1.winfo_width())
+
+button_1 = tk.Button(screen, width=500, bg='#ffc8dd',  height=100, bd=0, highlightthickness=0, relief='ridge')
