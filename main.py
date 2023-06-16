186deaae3a926bc121a5d5e05a20de829c6be9f7
josevictormoreno <josevictor.admoreno@gmail.com>
2023-06-15 23:51:58 -0300

    corrige erro dos botões

--- main.py
+++ main.py
@@ -6,25 +6,27 @@ import random
 screen = tk.Tk()
 screen.title("ACEITE!")
 screen.geometry("600x600")
-screen.configure(background='#ffc8dd')
+screen.configure(background='#ffffff')
 
 def move_button_1(e):
     if abs(e.x - button_1.winfo_x()) < 50 and abs(e.y - button_1.winfo_y()) < 40:
         x = random.randint(0, screen.winfo_width() - button_1.winfo_width())
+        y = random.randint(0, screen.winfo_height() - button_1.winfo_height())
+        button_1.place(x=x, y=y)
 
 def accepted():
-    messagebox.showinfo('OBA!')
+    messagebox.showinfo('CORRETO!')
 def denied():
     button_1.destroy()
 
-margin = Canvas(screen, width=500, bg='#ffc8dd', height=100, bd=0, highlightthickness=0, relief='ridge')
+margin = Canvas(screen, width=500, bg='#FFFFFF', height=100, bd=0, highlightthickness=0, relief='ridge')
 margin.pack()
-text_id = Label(screen, bg='#ffc8dd', text='quer namorar comigo?', fg='#590d2', font=('Arial', 24, 'bold'))
+text_id = Label(screen, bg='#ffffff', text='O zeviane é um babaca?', fg='#000000', font=('arial', 30, 'bold'))
 text_id.pack()
-button_1 = tk.Button(screen, width=500, bg='#ffc8dd',  height=100, bd=0, highlightthickness=0, relief='ridge')
+button_1 = tk.Button(screen, text='Não', bg='#909090', bd=3, command=denied, relief=RIDGE, font=('arial', 10, 'bold'))
 button_1.pack()
 screen.bind('<Motion>', move_button_1)
-button_2 = tk.Button(screen, text='Sim', bg='#ffb3c1', relief=RIDGE,  bd=3, command=accepted, font=('Arial', 24, 'bold'))
+button_2 = tk.Button(screen, text='Sim', bg='#909090', relief=RIDGE,  bd=3, command=accepted, font=('arial', 10, 'bold') )
 button_2.pack()
 
 screen.mainloop()
